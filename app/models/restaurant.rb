8d708bae044c9c1c009c85c4153d977d39c90c3d
Zach Lawson <zachlawson@hotmail.com>
2014-11-13 14:14:32 -0500

    Cleaned up the restaurant controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,4 @@
 class Restaurant < ActiveRecord::Base
 has_many :reservations
-<<<<<<< HEAD
-
-
-=======
 has_many :customers, through: :reservations
->>>>>>> 47d3b9526b9c10b390e7bb2524c3b525bda036e2
 end
