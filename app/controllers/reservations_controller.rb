26527cca6481b7a445ba7f35efe39e8f72a32c54
Amy Rudolph <amy.rudolph@live.co.uk>
2014-11-12 22:25:30 -0500

    created reservation controller with show create and destroy. I defined some methods in the controller and a before filter. im not sure which or how many of the methods i can move to the model.

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,27 @@
+class ReservationsController < ApplicationController
+	before_filter :ensure_loged_in
+
+  def show
+  	@reservation = Reservation.find(params[:id])
+  end
+
+  def create
+  	@reservation = @restaurant.reservation.build(reservation_params)
+  	# @reservation.customer = current_customer
+  	# if @reservation.save                
+  	# 	redirect_to confirmation_path  #need to create a confirmation page and path or we just redirect to restaurant list and write a notice that the restervation has been saved
+  	# else
+  	# 	render "restaurants"
+  	# end
+  end
+
+  def destroy
+  	@reservation = Reservation.find(params[:id])
+  	@reservation.destroy
+  end
+
+  private
+  def reservation_params
+  	params.require(:reservation).permit(:reservation_time, :restaurant_id, :customer_id)
+  end
+end
